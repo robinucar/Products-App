cdcb89e8da64792b9537b0cf3ecf25c2010f9b92
Robin <mehmet.ucar@imediapro.co.uk>
2020-05-21 20:28:11 +0100

    projects set up and components

---
+++ src/Components/Products/Products.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Products extends Component {
+  render() {
+    return (
+      <div>
+          <h3>This is the products list.</h3>
+      </div>
+    )
+  }
+}
+
+export default Products;
