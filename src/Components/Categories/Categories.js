f4301a50990c51d25153ddc4a04f8666b0b1a229
Robin <mehmet.ucar@imediapro.co.uk>
2020-05-22 23:04:28 +0100

    rest api setted up

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -11,6 +11,15 @@ class Categories extends Component {
 
   };
 
+  componentDidMount() {
+    this.getCategories()
+  }
+
+  getCategories = () => {
+    fetch('http://localhost:3000/categories')
+    .then(response => response.json())
+    .then(data => this.setState({categories: data}))
+  };
 
 
 
